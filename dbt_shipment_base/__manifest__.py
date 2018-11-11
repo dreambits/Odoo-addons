c2865b44bff874a3afe8b67f888cef5c0de3b863
dreambits <admin@dreambits.in>
2018-11-11 12:59:15 +0530

    Fixed dependencies for shipment base due to change in module structure for odoo 12.0

--- dbt_shipment_base/__manifest__.py
+++ dbt_shipment_base/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Dreambits Shipments Base',
     'version': '12.0.1.0.0',
-    'depends': ['base', 'sale_stock'],
+    'depends': ['sale_management','stock'],
     'summary': 'Module to have Shipment and Shipment Transporter Models',
     'description': """
         This module includes shipment and shipment transporter models which will
