af5a1663f3efd9b31861daf212decee834426c1f
Devang@dreambits <devang@dreambits.in>
2018-12-31 22:16:02 +0530

    [FIX] fixed latest order cound and view; fixed booleans used in different method; added view for shipment latest & pending order; fixed dashboard view by removing unused code and field

--- dbt_marketplace_base/models/base_template.py
+++ dbt_marketplace_base/models/base_template.py
@@ -19,7 +19,6 @@ class DbtMarketplaceBase(models.Model):
 
     name = fields.Char('Name')
     active = fields.Boolean('Active')
-    connected = fields.Boolean('Connection Tested')
 
     latest_order_count = fields.Integer("New Order Count", compute="get_latest_order_count")
 
@@ -40,12 +39,14 @@ class DbtMarketplaceBase(models.Model):
 
     @api.depends('child_class_id','child_class_name')
     def get_latest_order_count(self):
+        self.log("Inside get_latest_order_count for marketplace")
         try:
             for rec in self:
                 if rec.child_class_name and rec.child_class_id:
                     rec.latest_order_count = rec.env[rec.child_class_name].browse(
                             rec.child_class_id
-                        ).bol_instance.latest_order_count
+                        ).get_latest_order_count()
+                self.log("Inside get_latest_order_count for marketplace rec.latest_order_count-> {0}".format(rec.latest_order_count))
         except:
             traceback.print_exc()
 
@@ -61,6 +62,8 @@ class DbtMarketplaceBase(models.Model):
                 self.log("Child class found")
                 self.env[self.child_class_name].browse(
                     self.child_class_id).with_delay().fetch_orders()
+                self.get_latest_order_count()
+                self.log("Inside fetch_orders for marketplace self.latest_order_count-> {0}".format(self.latest_order_count))
         except:
             traceback.print_exc()
 
@@ -69,7 +72,7 @@ class DbtMarketplaceBase(models.Model):
         try:
             self.log("Inside sync_from_products_action for marketplace")
             self.log(self.enable_order_fetching)
-            if self.enable_order_fetching and self.child_class_name and self.child_class_id:
+            if self.enable_product_from_sync and self.child_class_name and self.child_class_id:
                 self.log("Function calling starts")
                 self.log(self.child_class_name)
                 self.log(self.child_class_id)
@@ -84,7 +87,7 @@ class DbtMarketplaceBase(models.Model):
         try:
             self.log("Inside sync_to_products_action for marketplace")
             self.log(self.enable_order_fetching)
-            if self.enable_order_fetching and self.child_class_name and self.child_class_id:
+            if self.enable_product_to_sync and self.child_class_name and self.child_class_id:
                 self.log("Function calling starts")
                 self.log(self.child_class_name)
                 self.log(self.child_class_id)
@@ -99,7 +102,7 @@ class DbtMarketplaceBase(models.Model):
         try:
             self.log("Inside sync_shipments_action for marketplace")
             self.log(self.enable_order_fetching)
-            if self.enable_order_fetching and self.child_class_name and self.child_class_id:
+            if self.enable_shipment_sync  and self.child_class_name and self.child_class_id:
                 self.log("Function calling starts")
                 self.log(self.child_class_name)
                 self.log(self.child_class_id)
@@ -127,14 +130,30 @@ class DbtMarketplaceBase(models.Model):
 
     ## The functions needed to open appropriate window for view actions of dashboard ##
     def view_all_orders(self):
-        self.log("Inside view all orders: base")
+        self.log("Inside view all orders: marketplace")
         # raise NotImplementedError('Must be overrided in child class')
         if self.child_class_name and self.child_class_id:
             return self.env[self.child_class_name].browse(
                 self.child_class_id).view_all_orders()
 
+    ## The functions needed to open appropriate window for view actions of dashboard ##
     def view_pending_orders(self):
-        raise NotImplementedError('Must be overrided in child class')
+        # raise NotImplementedError('Must be overrided in child class')
+        if self.child_class_name and self.child_class_id:
+            return self.env[self.child_class_name].browse(
+                self.child_class_id).view_pending_orders()
 
+    ## The functions needed to open appropriate window for view actions of dashboard ##
     def view_shipment(self):
-        raise NotImplementedError('Must be overrided in child class')
+        # raise NotImplementedError('Must be overrided in child class')
+        self.log("Inside view shipment: marketplace")
+        if self.child_class_name and self.child_class_id:
+            return self.env[self.child_class_name].browse(
+                self.child_class_id).view_shipment()
+
+    ## The functions needed to open appropriate window for view actions of dashboard ##
+    def view_latest_orders(self):
+        self.log("Inside view latest orders: marketplace")
+        if self.child_class_name and self.child_class_id:
+            return self.env[self.child_class_name].browse(
+                self.child_class_id).view_latest_orders()
